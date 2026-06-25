26a5218fffecdd38196f56b3f32829def07fc09e
Ashutosh Kumar <kmashutosh12kr@gmail.com>
2026-06-25 14:16:27 +0530

    Time: 5 ms (26.78%), Space: 11.7 MB (31.05%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,40 +2,23 @@ class Solution {
 public:
     string predictPartyVictory(string senate) {
         int n = senate.length();
-        queue<int> ans;
         queue<int> r;
         queue<int> d;
         for (int i = 0; i < n; i++) {
-            ans.push(i);
             if (senate[i] == 'R')
                 r.push(i);
             else
                 d.push(i);
         }
-        while(ans.size()>1){
-            if(senate[ans.front()]=='X') ans.pop();
-            else if(senate[ans.front()]=='R'){
-                if(d.size()==0) return "Radiant"; 
-                else if(d.size()>0){  /// take rights of next d
-                    senate[d.front()]='X';
-                    d.pop();
-                    ans.push(ans.front());
-                    ans.pop();
-                    r.push(r.front());
-                    r.pop();
-                }
+        while(r.size()>0 && d.size()>0){
+            int ri=r.front();
+            int di=d.front();
+            r.pop();
+            d.pop();
+            if(ri<di){
+                r.push(ri+n);
             }
-            else if(senate[ans.front()]=='D'){
-                if(r.size()==0) return "Dire"; 
-                else if(r.size()>0){   /// take rights of next r
-                    senate[r.front()]='X';
-                    r.pop();
-                    ans.push(ans.front());
-                    ans.pop();
-                    d.push(d.front());
-                    d.pop();
-                }
-            }
+            else d.push(di+n);
         }
         return (r.size()>0)? "Radiant":"Dire";
     }
