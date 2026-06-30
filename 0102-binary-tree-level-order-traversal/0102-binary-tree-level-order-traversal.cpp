54790565f9f2e05cc0eb83c9eae12b52d80da5bd
Ashutosh Kumar <kmashutosh12kr@gmail.com>
2026-06-30 17:51:04 +0530

    Time: 3 ms (26.28%), Space: 16.8 MB (96.39%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,15 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     int level(TreeNode* root) {
@@ -17,28 +5,20 @@ public:
             return 0;
         return 1 + max(level(root->left), level(root->right));
     }
-    void nthlevel(TreeNode* root, int curr, int level,vector<int>& v) {
-        if (root == NULL)
-            return;
-        if (curr == level) {
-            v.push_back(root->val);
-            return;
-        }
-        nthlevel(root->left, curr + 1, level,v);
-        nthlevel(root->right, curr + 1, level,v);
-    }
-    void lOrder(TreeNode* root,vector<vector<int>>& ans) {
-        int n = level(root);
-        for (int i = 1; i <= n; i++) {
-            vector<int>v;
-            nthlevel(root, 1, i,v);
-            ans.push_back(v);
-            cout<<endl;
-        }
+    void lOrder(TreeNode* root,vector<vector<int>>& ans,int level) {
+        if(root==NULL) return;
+        ans[level].push_back(root->val);
+        lOrder(root->left,ans,level+1);
+        lOrder(root->right,ans,level+1); 
     }
     vector<vector<int>> levelOrder(TreeNode* root) { 
         vector<vector<int>> ans; 
-        lOrder(root,ans);
+        int n = level(root);
+        for(int i=1;i<=n;i++){
+            vector<int>v;
+            ans.push_back(v);  
+        }
+        lOrder(root,ans,0); 
         return ans;
         }
 };
