6d9623db5b38acb8de6016eb81468102977a9145
Ashutosh Kumar <kmashutosh12kr@gmail.com>
2026-07-02 23:11:54 +0530

    Time: 4 ms (61.11%), Space: 124.6 MB (64.41%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-     vector<int>ans;
      int idx=1;
      int fidx=-1;
      int sidx=-1;
+     int mindis=INT_MAX;
+     int f=-1;
+     int s=-1;   
      ListNode* prev=head;   
      ListNode* curr=head->next; 
      while(curr->next!=NULL){
@@ -12,8 +14,14 @@ public:
         int b=curr->val;
         int c=curr->next->val;
         if((b<a && b<c)|| (b>a && b>c)){
+            ///maxdistance
             if(fidx==-1) fidx=idx;
             else sidx=idx;
+            //mindistance
+            f=s;
+            s=idx;
+            if(f!=-1) mindis=min(mindis,s-f);
+
         }
             prev=prev->next;
             curr=curr->next;
@@ -22,25 +30,7 @@ public:
      if(sidx==-1){
         return {-1, -1};
      }
-     int mindis=INT_MAX;
      int maxdis=sidx-fidx;
-     fidx=-1;
-     sidx=-1;
-     prev=head;   
-     curr=head->next;
-     while(curr->next!=NULL){
-        int a=prev->val;
-        int b=curr->val;
-        int c=curr->next->val;
-        if((b<a && b<c)|| (b>a && b>c)){
-            fidx=sidx;
-            sidx=idx;
-            mindis=min(mindis,sidx-fidx);
-        }
-            prev=prev->next;
-            curr=curr->next;
-            idx++;
-     }
      return {mindis,maxdis}; 
     }
 };
