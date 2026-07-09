d84529ca28620bb94b17ab0595b56ce33b79e3bd
Ashutosh Kumar <kmashutosh12kr@gmail.com>
2026-07-09 18:59:29 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.7%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+    unordered_map<char,int>mp;
+    for(int i=0;i<s.length();i++){
+        mp[s[i]]++;
+    }
+    for(int i=0;i<t.length();i++){
+        char ch=t[i];
+        if(mp.find(ch)!=mp.end()) {
+            mp[ch]--; 
+            if(mp[ch]==0) mp.erase(ch);
+        } 
+        else return false;
+    }
+    if(mp.size()>0) return false;
+    else return true;
+    }
+};
