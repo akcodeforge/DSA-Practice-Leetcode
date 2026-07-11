abd7e06dd09e72da29385a744bae3c20ec26b716
Ashutosh Kumar <kmashutosh12kr@gmail.com>
2026-07-11 18:12:48 +0530

    Time: 7 ms (51.8%), Space: 15.3 MB (8.46%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -11,44 +11,26 @@ public:
             temp=temp->next;
             deepC=deepC->next;
         }
-        Node* duplicate=deep->next;
+        Node* b=deep->next;
         Node* a=head;
-        Node* b=duplicate;
-        deep=new Node(-1);
-        Node* deepD=deep;
-        while(b){
-            deepD->next=a;
-            deepD=deepD->next;
-            a=a->next;
-            deepD->next=b;
-            deepD=deepD->next;
-            b=b->next;
+        /// map a map with<original,duplicate>node;
+        unordered_map<Node*,Node*>mp;
+        Node* tempa=a;
+        Node* tempb=b;
+        while(tempa!=NULL){
+            mp[tempa]=tempb;
+            tempa=tempa->next;
+            tempb=tempb->next;
         }
-        deep=deep->next;
-        Node* t1=deep;// t1 traverse in original list
-        while(t1){
-            Node* t2=t1->next;
-            if(t1->random) t2->random=t1->random->next;
-            t1=t1->next->next; 
+        for(auto x:mp){
+            Node* o=x.first;
+            Node* d=x.second;
+            if(o->random!=NULL){/// kam karo
+                Node* oran=o->random;
+                Node* dran=mp[oran];
+                d->random=dran;
+            }
         }
-        // step4 remove the connection
-        Node* d1=new Node(-1);
-        Node* d2=new Node(-1);
-        t1=d1;
-        Node* t2=d2;
-        Node* t=deep;
-        while(t){
-            t1->next=t;
-            t=t->next;
-            t1=t1->next;
-            t2->next=t;
-            t=t->next;
-            t2=t2->next;
-        }
-        t1->next=NULL;
-        t2->next=NULL;
-        d1=d1->next; /// original with random
-        d2=d2->next;/// duplicate with random
-        return d2;
+        return b;
     }
 };
