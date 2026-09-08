206791ab9e3b7fc05cfcc2d600c9f71645d67371
Ashutosh Kumar <kmashutosh12kr@gmail.com>
2026-09-09 01:16:21 +0530

    Time: 221 ms (5.53%), Space: 23 MB (5.13%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int i,vector<int>&dp){ 
+        if(i==nums.size()-1) return 0;
+        
+        if(dp[i]!=-1) return dp[i];
+        int ans=INT_MAX;
+        for(int j=1;j<=nums[i] && i+j<nums.size();j++){
+            int x=solve(nums,i+j,dp);
+            if(x!=INT_MAX){
+            ans = min(ans,(1+x));
+            }
+        }
+        return dp[i]=ans;
+    }
+    int jump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return solve(nums,0,dp);
+    }
+};
